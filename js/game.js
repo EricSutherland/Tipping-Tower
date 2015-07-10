fc253e838f3cda9befcd963e24bf671e5ae18741
eric sutherland <esutherland04@hotmail.com>
2015-07-10 18:16:18 +0100

    creation of basic gameloop
    also created css for the canvas

--- js/game.js
+++ js/game.js
@@ -1,7 +1,33 @@
-window.onload = function() {
+var canvasContext = null
+    fps = 30,
+    interval     =    1000/fps,
+    lastTime     =    (new Date()).getTime(),
+    currentTime  =    0,
+    delta = 0,
+	canvasHeight = 0,
+	canvasWidth = 0;
+	
+
+window.onload = function() 
+{
 	var canvas = document.getElementById('game');
-    var context = canvas.getContext('2d');
-	context.font = '40pt Calibri';
-    context.fillStyle = 'blue';
-    context.fillText('Hello World!', 150, 100);
+	canvasContext = canvas.getContext('2d');
+	canvasHeight = canvas.height;
+	canvasWidth = canvas.width;
+	gameLoop();
 };
+
+
+function gameLoop() {
+    window.requestAnimationFrame(gameLoop);
+    
+    currentTime = (new Date()).getTime();
+    delta = (currentTime-lastTime);
+
+    if(delta > interval) 
+	{
+		canvasContext.clearRect(0,0,canvasHeight,canvasWidth);
+		console.log("looping");
+        lastTime = currentTime - (delta % interval);
+    }
+}
