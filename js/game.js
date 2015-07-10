b0a0e1b70ac9136970190165ef07dbd21ee610d8
eric sutherland <esutherland04@hotmail.com>
2015-07-10 18:58:54 +0100

    Basic moving floor
    the current floor moves from side to side and when touching the wall
    will change direction and move the other way.

--- js/game.js
+++ js/game.js
@@ -7,6 +7,15 @@ var canvasContext = null
 	canvasHeight = 0,
 	canvasWidth = 0;
 	
+// variables for the current floor
+var floorWidth = 300,
+	floorHeight = 40,
+	floorPosition = 0,
+	speed = 3,
+	targetPosition = 0;
+	
+	
+	
 
 window.onload = function() 
 {
@@ -14,6 +23,9 @@ window.onload = function()
 	canvasContext = canvas.getContext('2d');
 	canvasHeight = canvas.height;
 	canvasWidth = canvas.width;
+	
+	floorPosition = canvasWidth/2;
+	
 	gameLoop();
 };
 
@@ -27,7 +39,37 @@ function gameLoop() {
     if(delta > interval) 
 	{
 		canvasContext.clearRect(0,0,canvasHeight,canvasWidth);
-		console.log("looping");
+		update();
+		draw()
         lastTime = currentTime - (delta % interval);
     }
+}
+
+function update()
+{
+	updateCurrentFloor();
+}
+
+function draw()
+{
+	drawCurrentFloor();
+}
+
+function updateCurrentFloor()
+{
+	if (floorPosition + floorWidth >= canvasWidth || floorPosition <= 0) // if the floor has reached the edge of the screen
+	{
+		speed = speed - (speed * 2); //flips the direction the floor is travelling
+	}
+	
+	floorPosition += speed;
+}
+
+function drawCurrentFloor()
+{
+	canvasContext.beginPath();
+	canvasContext.rect(floorPosition,canvasHeight/2- floorHeight, floorWidth , 100);
+	canvasContext.fillStyle = 'blue';
+	canvasContext.fill();
+
 }
