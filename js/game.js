7be548b822ca413227236d86e3736fbdc50d7405
eric sutherland <esutherland04@hotmail.com>
2015-07-11 12:41:50 +0100

    Previous floors implemented
    previously places floors now stack as the player builds the tower

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+// variables for gameloop and drawing
 var canvasContext = null
     fps = 30,
     interval     =    1000/fps,
@@ -8,14 +9,18 @@ var canvasContext = null
 	canvasWidth = 0;
 	
 // variables for the current floor
-var floorWidth = 300,
-	floorHeight = 40,
-	floorPosition = 0,
-	speed = 3,
-	targetPosition = 0;
+var floor = {
+	width : 300,
+	height : 60,
+	position : 0,
+	speed : 3,
+};
+	targetPosition = 100;
+	marginOfError = 5;
 	
-	
-	
+var previousFloors = new Array(5);
+
+
 
 window.onload = function() 
 {
@@ -24,14 +29,15 @@ window.onload = function()
 	canvasHeight = canvas.height;
 	canvasWidth = canvas.width;
 	
-	floorPosition = canvasWidth/2;
+	floor.position = canvasWidth / 2;
 	
-	gameLoop();
+	canvas.addEventListener('click', PlaceFloor);
+	GameLoop();
 };
 
 
-function gameLoop() {
-    window.requestAnimationFrame(gameLoop);
+function GameLoop() {
+    window.requestAnimationFrame(GameLoop);
     
     currentTime = (new Date()).getTime();
     delta = (currentTime-lastTime);
@@ -39,37 +45,116 @@ function gameLoop() {
     if(delta > interval) 
 	{
 		canvasContext.clearRect(0,0,canvasHeight,canvasWidth);
-		update();
-		draw()
+		Update();
+		Draw()
         lastTime = currentTime - (delta % interval);
     }
 }
 
-function update()
+function Update()
 {
-	updateCurrentFloor();
+	UpdateCurrentFloor();
 }
 
-function draw()
+function Draw()
 {
-	drawCurrentFloor();
+	DrawCurrentFloor();
+	DrawDebugLines();
+	DrawPreviousFloors();
 }
 
-function updateCurrentFloor()
+function UpdateCurrentFloor()
 {
-	if (floorPosition + floorWidth >= canvasWidth || floorPosition <= 0) // if the floor has reached the edge of the screen
+	if (floor.position + floor.width >= canvasWidth || floor.position <= 0) // if the floor has reached the edge of the screen
 	{
-		speed = speed - (speed * 2); //flips the direction the floor is travelling
+		floor.speed = floor.speed - (floor.speed * 2); //flips the direction the floor is travelling
 	}
 	
-	floorPosition += speed;
+	floor.position += floor.speed;
 }
 
-function drawCurrentFloor()
+function DrawCurrentFloor()
 {
 	canvasContext.beginPath();
-	canvasContext.rect(floorPosition,canvasHeight/2- floorHeight, floorWidth , 100);
+	canvasContext.rect(floor.position,(canvasHeight / 2) - floor.height, floor.width , floor.height);
 	canvasContext.fillStyle = 'blue';
 	canvasContext.fill();
 
+}
+function DrawDebugLines()
+{
+	canvasContext.beginPath();
+    canvasContext.moveTo(targetPosition, (canvasHeight / 2) - floor.height);
+    canvasContext.lineTo(targetPosition, canvasHeight / 2);
+    canvasContext.stroke();
+}
+
+function DrawPreviousFloors()
+{
+	for ( i = 0 ; i < previousFloors.length ; i++)
+	{
+		if (previousFloors[i])
+		{
+			canvasContext.beginPath();
+			var temp = (canvasHeight / 2) + (floor.height *(i+1));
+			canvasContext.rect(previousFloors[i].position, (canvasHeight / 2) + (floor.height *(i)), previousFloors[i].width , floor.height);
+			canvasContext.fillStyle = 'blue';
+			canvasContext.fill();
+			canvasContext.lineWidth = 1;
+			canvasContext.strokeStyle = 'black';
+			canvasContext.stroke();
+		}
+	}
+}
+
+function PlaceFloor()
+{
+	var difference = Math.abs(floor.position - targetPosition);
+	console.log("difference", difference);
+	if (difference > marginOfError)
+	{
+		floor.width -= difference;
+		
+		if (floor.width <= 0)
+		{
+			// gameover
+			console.log("gameover");
+			return;
+		}
+		
+		if (targetPosition < floor.position)
+		{
+			targetPosition = floor.position;
+		}
+	}
+	else
+	{
+		floor.position = targetPosition;
+	}
+	
+	SetupPreviousFloors();
+	
+	floor.position = 1;
+	console.log(targetPosition);
+
+}
+
+function SetupPreviousFloors()
+{
+
+	previousFloors[4] = previousFloors[3];
+	previousFloors[3] = previousFloors[2];
+	previousFloors[2] = previousFloors[1];
+	previousFloors[1] = previousFloors[0];
+	
+	var tempPos = floor.position;
+	if (targetPosition > floor.position)
+		{
+			tempPos = targetPosition;
+		}
+		
+	previousFloors[0] = {
+		width : floor.width,
+		position : tempPos
+	};
 }
